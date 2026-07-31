bb5f6ea2ccaa682e5a0a15496145602b58f08f9e
sasha <sasha.friis@icloud.com>
2026-07-31 13:25:31 +0200

    align-regexp key map + space between functions

--- +map.el
+++ +map.el
@@ -1,15 +1,24 @@
 (map! :g "M-1" #'centaur-tabs-backward
       :g "M-2" #'centaur-tabs-forward)
+
 (map! :nv "M-<left>" #'+workspace/switch-left
       :nv "M-<right>" #'+workspace/switch-right)
+
 (setq +workspace-cycle-wrap t)
+
 (map! :leader
       :desc "Take a screenshot" "S" #'screenshot)
+
 (map! :n "C-;" #'embark-act)
+
 (map! :leader
       :desc "Aphelia buffer on/off" "A" #'apheleia-mode)
+
 (map! :leader
       :desc "Live preview" "o v" #'httpd-serve-directory)
+
+(map! :map meow-normal-state-keymap
+      :desc "align-regexp" "`" #'align-regexp)
 
 (after! meow
   (meow-normal-define-key
